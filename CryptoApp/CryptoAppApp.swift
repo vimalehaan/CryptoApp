0c3e06f79c2f02ac7e29f5e407add71eddcc8f88
vimalehaan <vimalehaan2312@mail.com>
2024-08-26 18:15:11 +0530

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Lehaananth Vimalanathan on 2024-08-26.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
